f2ae8d62d70e51b754449b0341d92365d05dfe94
Kawisaralekkie <160382578+Kawisaralekkie@users.noreply.github.com>
2024-02-26 13:53:38 +0700

    Exercise 14
    To create exercise by following the assignment

---
+++ Exercise4_Kawisara_W.py
@@ -0,0 +1,10 @@
+print("--- Your Score ---")
+Found_Eng = 80
+Gen_Bus = 84.5
+Intro_Com_Sys = 90
+Com_pro = 88.4
+print("Foundation English :",Found_Eng)
+print("General Business :",Gen_Bus)
+print("Introduction to Computer System :",Intro_Com_Sys)
+print("Computer Programming :",Com_pro)
+
